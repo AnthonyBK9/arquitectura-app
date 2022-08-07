0da55727070a8469b8159a57a3d3fafe1f4db581
AnthonyBK9 <antonio.bermudez.ing@gmail.com>
2022-08-07 18:21:12 -0500

    Terminando la sección de Sobre nosotros

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import AboutMe from './components/aboutMe/AboutMe'
 import Header from './components/header/Header'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <AboutMe />
     </div>
   )
 }
--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import './style/aboutme.css'
 
 const AboutMe = () => {
   return (
-    <div>AboutMe</div>
+    <section className="container mt-5 aboutMe">
+      <div className="aboutMe-content">
+        <h2>Sobre Nosotros</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, consectetur. Voluptates deleniti nobis ad doloremque, totam eos? Sit deserunt odit aut, dicta nihil inventore ipsam hic. Eius hic accusamus ratione.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas maiores sunt praesentium impedit, inventore corrupti ratione, velit quidem dignissimos ad repellendus enim amet eveniet modi sint perferendis! Officiis, suscipit sequi.</p>
+      </div>
+    </section>
   )
 }
 
